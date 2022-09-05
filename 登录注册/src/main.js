9466ae5e84017fbb23d83b531b0c3ec65e7e3ee8
bunny <13199000154@qq.com>
2022-09-05 11:12:16 +0800

    LoginorRegister-day1

--- 登录注册/src/main.js
+++ 登录注册/src/main.js
@@ -3,4 +3,5 @@ import "./css/reset.css"
 import "./css/mixins/clear.less"
 import "./js/data.js"
 import "./js/index.ts"
+// import "./favicon.ico"
 import "core-js/es/promise"; //更好的方式，减少体积，直打包专一的---promise
